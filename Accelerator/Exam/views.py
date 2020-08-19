f40a3f0c196acab6429a1079da1af773aac7fc06
rbala2 <34136719+rbala2@users.noreply.github.com>
2020-08-18 23:33:41 -0400

    User/bala/exam fixes (#19)
    * Added test desc as test heading  as  and added timer auto submit
    
    * Fixed a bug from accessing test after attempt using direct url and cookie manipulation

--- Accelerator/Exam/views.py
+++ Accelerator/Exam/views.py
@@ -112,18 +112,23 @@ class QuestionsView(ListView):
     def get_context_data(self, **ctx_kwargs):
         context = super().get_context_data(**ctx_kwargs)
         context['ques_cnt'] = self.kwargs['ques_cnt']
+        context['exam_desc'] = self.request.COOKIES['exam_desc']
+        context['exam_dur'] = self.request.COOKIES['exam_dur']
         return context
 
     def render_to_response(self, context, **response_kwargs):
         response = super(QuestionsView, self).render_to_response(context, **response_kwargs)
         response.set_cookie("test_id", self.kwargs['test_id'])
-        if self.request.COOKIES['exam_dur'] == '-1':
+        # response.set_cookie("exam_desc", self.request.COOKIES['exam_desc'])
+        tobj = AccStudentTests.objects.get(student_id=self.request.user.id, test_id=self.kwargs['test_id'])
+        # if self.request.COOKIES['exam_dur'] == '-1':
+        if tobj.test_status == 'Attempted':
             return redirect('exam-home')
         else:
-            tobj = AccStudentTests.objects.get(student_id=self.request.user.id, test_id=self.kwargs['test_id'])
+            # tobj = AccStudentTests.objects.get(student_id=self.request.user.id, test_id=self.kwargs['test_id'])
             tobj.test_status = 'Attempted'
             tobj.save()
-            response.set_cookie("exam_dur", '-1')
+            # response.set_cookie("exam_dur", '-1')
         return response
 
     def get_queryset(self):
