7c5876d95e4373a90c63b6fa74689c246688de4a
rbala2 <34136719+rbala2@users.noreply.github.com>
2020-09-05 09:50:18 -0400

    Adjust for Heroku prod deployment

--- Accelerator/Accelerator/urls.py
+++ Accelerator/Accelerator/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', include('PortalHome.urls')),
+    path('', include('PortalHome.urls')),
     path('Exam/', include('Exam.urls')),
 ]
